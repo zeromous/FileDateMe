d13735a157b609923e8b4ecf0dda16f44214ca58
zeromous <karl.koelbel@gmail.com>
2025-03-01 22:02:02 -0500

    Update FileDateMe21.ps1

--- FileDateMe21.ps1
+++ FileDateMe21.ps1
@@ -8,7 +8,7 @@
 # Usage: .\FileDateMe2.ps1 -directoryPath <path> [-dryRun] [-backup] [-nometa-use-mdate] [-quiet] [-yes] 
 # >>> Directory Path is Required <<<
 # - Script will *only* match/change JPEG, JPG or PNG files in a directory
-# - Files that do not have a "Date Taken" metadata will be skipped and can use [-nometa-use-mdate] to use the modified date
+# - Files that do not have a "Date Taken" metadata will be skipped and can use `[-nometa-use-mdate]` to use the modified date
 #
 # >>> WARNING <<<<
 # > `-yes` will automatically rename the files without asking for confirmation
@@ -16,7 +16,7 @@
 # > datestamp found in the original filename.
 # 
 # Default Usage:
-# - Preview Mode is Default
+# - Preview Mode is Default but will ask if you wish to proceed with changes
 # - This script will rename the files in the specified directory based on the "Date Taken" metadata of 
 # the image files.  This is useful when moving older photos without a date prefix on the filename, and
 # where cdate and mdate are not reliable.
